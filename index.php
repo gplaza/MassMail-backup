3245b7254d89387cc34eb1ce6bda0ba0b1275424
gplaza <gilles.grandguillaume@gmail.com>
2014-02-15 18:44:58 -0300

    Integration :
    - Bootstrap HTML boilerplate
    - AjaxQ github version
    - swift php mailer 5.0.3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include('utils.php');
 include('inc.config.php');
 include("db.class.php");
-include("entity.class.php"); 
+include("entity.class.php");
 
 $limited = 'limited';
 $complete = 'complete';
@@ -11,22 +11,22 @@ $complete = 'complete';
 $view = $limited;
 
 if(isset($_GET['view']))
-	$view = $_GET['view'];
+    $view = $_GET['view'];
 
 if(isset($_FILES['fichier']))
 {
-	deltree($basePath);
-	mkdir($basePath);
-	move_uploaded_file($_FILES['fichier']['tmp_name'], $basePath.basename($_FILES['fichier']['name']));
-	$fileReport =  $_FILES['fichier']['name'];
+    deltree($basePath);
+    mkdir($basePath);
+    move_uploaded_file($_FILES['fichier']['tmp_name'], $basePath.basename($_FILES['fichier']['name']));
+    $fileReport =  $_FILES['fichier']['name'];
 
-	include "zip/pclzip.lib.php";
-	$path=$basePath.$fileReport;
-	$archive = new PclZip($path);
-	$result = $archive->extract(PCLZIP_OPT_PATH, $basePath);
+    include "zip/pclzip.lib.php";
+    $path=$basePath.$fileReport;
+    $archive = new PclZip($path);
+    $result = $archive->extract(PCLZIP_OPT_PATH, $basePath);
 
-	checkFileName($basePath);
-	unlink($path);
+    checkFileName($basePath);
+    unlink($path);
 }
 
 ?>
@@ -38,147 +38,254 @@ if(isset($_FILES['fichier']))
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>MassMail</title>
 <link rel="stylesheet" type="text/css" href="css/jquery.tagit.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" />
 <link rel="stylesheet" type="text/css" href="css/site.css" />
-<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/humanity/jquery-ui.css" />
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript" ></script>
-<script charset="utf-8" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/jquery-ui.min.js" type="text/javascript" ></script>
-<script type="text/javascript" src="js/jquery.ajaxq-0.0.1.js"></script>
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"
+	type="text/javascript"></script>
+<script charset="utf-8"
+	src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/jquery-ui.min.js"
+	type="text/javascript"></script>
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/ajaxq.js"></script>
 <script type="text/javascript" src="js/tag-it.js"></script>
-<script type="text/javascript" src="js/massmail.min.js"></script>
+<script type="text/javascript" src="js/massmail.js"></script>
 
 </head>
 
-<body style="background:#fde2a0;font-family:Arial,Helvetica,sans-serif">
-
-<div>
-	<img src="img/logo.png" alt="logo" />
-	<div class="main" >
-		<div id="tabs" >
-
-			<ul>
-				<li><a href="#tabs-1">Mandar</a></li>
-				<li><a href="#tabs-2">Ficheros</a></li>
-				<li><a href="#tabs-3">Parametros</a></li>
-			</ul>
-			<div id="tabs-1">
-<?php
-
-if(isset($_POST['empresaSearchId']))
-	$search = $conn->query("SELECT * from empresa where empresa.id=".$_POST['empresaSearchId']);
-else
-	$search = $conn->query("SELECT * from empresa");
-	
-$result = $search->fetchAll(PDO::FETCH_ASSOC);
-$entities = array();
-
-foreach ($result as $empresa) {
-	
-	$files = CheckDirectory($empresa['nombre'],$basePath);
-	
-	if (count($files) > 0 || $view == $complete || isset($_POST['empresaSearchId']))
-	{
-		$entity=new entity();
-		$entity->setName($empresa['nombre']);
-		$entity->setFiles($files);
-		$entity->setId($empresa['id']);
-		$entity->setMails($conn->query('select mail.email from empresa_mail,mail WHERE empresa_mail.id_empresa = '.$empresa['id'].' AND mail.id = empresa_mail.id_mail')->fetchAll(PDO::FETCH_ASSOC));
-		$entities[] = $entity;
-	}
-}
-?>
-				<form method="get" id="form-select" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-					<fieldset style="padding:5px;">
-						<?php 
-						if ($view == $limited) {
-							$buttonText = 'Mostrar Todo';
-							$valueHidden = $complete;
-						} else {
-							$buttonText = 'Mostrar solo con Archivo';
-							$valueHidden = $limited;
-						}
-						?>
-						<button type="button" id="actionSend" onclick="javascript:send('send.php')">Mandar</button>
-						<button type="button" id="actionAdd" onclick="javascript:add('empresas')">Agregar Empresa</button>
-						<input type="hidden" name="view" value="<?php echo $valueHidden ?>" />
-						<button type="submit" id="changeView" value="<?php echo $buttonText ?>"><?php echo $buttonText ?></button>
-						<?php echo 'Total : '.count($entities); ?>
-					</fieldset>
-				</form>
-				<fieldset id="loading_send" style="display:none;padding: 10px;">
-					<div id="progressbar" style="float:left;width:700px;margin-right:15px;" > </div><button type="button" onclick="javascript:cancelSend()" id="cancelSend">Cancelar todo</button>
-				</fieldset>								
-				<form method="post" id="form-search" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-					<fieldset style="padding:5px;">
-						<legend>Busqueda</legend>
-						<label>Empresa : </label>
-						<input type="hidden" id="empresa_search_id" name="empresaSearchId" value="" />
-						<input type="text" id="empresa_search" name="empresaSearch" value="" />
-						<button type="submit" id="search" value="Buscar">Buscar</button>
-					</fieldset>
-				</form>
-				
-				<table id="empresas" style="width: 1024px;margin: auto;">
-					<thead>
-						<tr>
-							<th>Empresa</th>
-							<th>recipientes</th>
-							<th>files</th>
-							<th>mandar<input type="checkbox" id="selectAllLine"  /></th>
-							<th>Estado</th>
-							<th>Acci&oacute;n</th>
-						</tr>
-					</thead>
-					<tbody>
-<?php
-
-foreach($entities as &$entity)
-{
-	$files = $entity->getFiles();
-	$mails = $entity->getMails();
-	$id = $entity->getId();
-?>
-
-						<tr id="line<?php echo $id; ?>">
-							<td style="white-space:nowrap;" id="entity<?php echo $id; ?>">
-								<?php echo $entity->getName(); ?>
-								<input id="empresa<?php echo $id; ?>" type="hidden" value="<?php echo $entity->getName(); ?>" />
-							</td>
-							<td><ul id="mails<?php echo $id; ?>"><?php foreach($mails as &$mail) echo '<li>'.$mail['email'].'</li>'; ?></ul></td>
-							<td style="font-size:9;"><ul id="files<?php echo $id; ?>"><?php foreach($files as &$file) echo $file;?></ul></td>
-							<td align="center" ><input type="checkbox" name="send[]" id="send<?php echo $id; ?>" /></td>
-							<td align="center" ><img src="img/ajax-loader.gif" style="display:none;" id="load<?php echo $id; ?>" alt="loader" /></td>
-							<td align="center" style="width:100px;" >
-								<button onclick="javascript:changeRow('<?php echo $id; ?>');" class="edit">Editar</button>
-								<button onclick="javascript:saveRow('<?php echo $id; ?>','save.php');" class="save">Grabar</button>
-								<button onclick="javascript:deleteRow('<?php echo $id; ?>','delete.php');" class="delete">Quitar</button>
-							</td>
-						</tr>
-<?php } ?>
-					</tbody>
-				</table>
+<body role="document">
+	<div role="navigation" class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">MassMail</a>
 			</div>
-			<div id="tabs-2">
-				<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
-					<fieldset style="font-size: 12px">
-						<legend>Subir nuevo archivo</legend>
-							<p>Archivo :
-							<input type="file" name="fichier" /></p>
-							<input type="submit" value="Subir" />
-					</fieldset>
-				</form>
-			</div>
-			<div id="tabs-3">
-				<fieldset style="font-size: 12px">
-					<legend>Parametros</legend>
-					<label>E-mail :</label><input type="text" name="From" value="<?php echo $From ?>" /><br />
-					<label>Usuario :</label><input type="text" name="Username" value="<?php echo $Username ?>" /><br />
-					<label>Password :</label><input type="password" name="Password" value="<?php echo $Password ?>" /><br />
-					<label>Mesaje :</label><textarea rows="15" cols="40" name="Body" ><?php echo $Body ?></textarea><br />
-				</fieldset>
+			<div class="navbar-collapse collapse">
+				<ul id="tabs" class="nav navbar-nav">
+					<li class="active"><a id="tab1Link" href="#tabs-1">Lista</a></li>
+					<li><a id="tab2Link" href="#tabs-2">Ficheros</a></li>
+					<li><a id="tab3Link" href="#tabs-3">Parametros</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
-</div>
+
+		<div class="container-fluid" id="tabs-1">
+			<?php
+
+			if(isset($_POST['empresaSearchId']))
+			    $search = $conn->query("SELECT * from empresa where empresa.id=".$_POST['empresaSearchId']);
+			else
+			    $search = $conn->query("SELECT * from empresa");
+
+			$result = $search->fetchAll(PDO::FETCH_ASSOC);
+			$entities = array();
+
+			foreach ($result as $empresa) {
+
+	$files = CheckDirectory($empresa['nombre'],$basePath);
+
+	if (count($files) > 0 || $view == $complete || isset($_POST['empresaSearchId']))
+	{
+	    $entity=new entity();
+	    $entity->setName($empresa['nombre']);
+	    $entity->setFiles($files);
+	    $entity->setId($empresa['id']);
+	    $entity->setMails($conn->query('select mail.email from empresa_mail,mail WHERE empresa_mail.id_empresa = '.$empresa['id'].' AND mail.id = empresa_mail.id_mail')->fetchAll(PDO::FETCH_ASSOC));
+	    $entities[] = $entity;
+	}
+}
+?>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Empresas</div>
+
+				<div class="panel-body">
+
+					<!-- <div class="row">		 -->
+					<form class="col-lg-2" method="post" id="form-search"
+						action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+						<div class="input-group">
+
+							<input type="hidden" id="empresa_search_id"
+								name="empresaSearchId" value="" /> <input type="text"
+								class="form-control" id="empresa_search"
+								placeholder="Empresa..." name="empresaSearch" value="" /> <span
+								class="input-group-btn">
+								<button type="submit" id="buscarButton" class="btn btn-primary">
+									<span class="glyphicon glyphicon-search"></span> Buscar
+								</button>
+							</span>
+
+						</div>
+						<!-- /input-group -->
+
+					</form>
+
+					<form method="get" id="form-select"
+						action="<?php echo $_SERVER['PHP_SELF']; ?>">
+						<?php 
+						if ($view == $limited) {
+							$buttonText = 'Mostrar Todo';
+							$imgIcon = 'glyphicon glyphicon-eye-open';
+							$valueHidden = $complete;
+						} else {
+							$buttonText = 'Mostrar solo con Archivo';
+							$imgIcon = 'glyphicon glyphicon-eye-close';
+							$valueHidden = $limited;
+						}
+						?>
+
+						<p>
+							<button type="button" class="btn btn-primary" id="actionSend"
+								onclick="javascript:send('send.php')">
+								<span class="glyphicon glyphicon-send"></span> Mandar
+							</button>
+							<button type="button" class="btn  btn-primary" id="actionAdd"
+								onclick="javascript:add('empresas')">
+								<span class="glyphicon glyphicon-plus-sign"></span> Agregar
+								Empresa
+							</button>
+							<input type="hidden" name="view"
+								value="<?php echo $valueHidden ?>" />
+							<button type="submit" class="btn btn-primary" id="changeView"
+								value="<?php echo $buttonText ?>">
+								<span class="<?php echo $imgIcon ?>"></span>
+								<?php echo $buttonText ?>
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="badge"><?php echo count($entities); ?>
+								</span>
+							</button>
+
+
+						</p>
+					</form>
+
+					<!--  
+					<p>
+						<div style="display: none;" id="loading_send"
+							class="progress progress-striped active">
+							<div class="progress-bar" role="progressbar" aria-valuenow="45"
+								aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+								<span class="sr-only">45% Complete</span>
+							</div>
+
+						</div>
+						<button style="display: none;" type="button"
+							class="btn btn-primary" onclick="javascript:cancelSend()"
+							id="cancelSend">Cancelar todo</button>
+					</p>
+					-->
+
+				</div>
+
+
+				<table class="table table-striped" id="empresas">
+					<thead>
+						<tr>
+							<th class="text-center">Empresa</th>
+							<th class="text-center">Recipientes</th>
+							<th class="text-center">Archivos</th>
+							<th class="text-center">Mandar<input type="checkbox" id="selectAllLine" /></th>
+							<th class="text-center">Estado</th>
+							<th class="text-center">Acci&oacute;n</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+
+						foreach($entities as &$entity)
+						{
+						    $files = $entity->getFiles();
+						    $mails = $entity->getMails();
+						    $id = $entity->getId();
+						    ?>
+
+						<tr id="line<?php echo $id; ?>">
+							<td style="white-space: nowrap;" id="entity<?php echo $id; ?>"><?php echo $entity->getName(); ?>
+								<input id="empresa<?php echo $id; ?>" type="hidden"
+								value="<?php echo $entity->getName(); ?>" />
+							</td>
+							<td><ul id="mails<?php echo $id; ?>">
+									<?php foreach($mails as &$mail) echo '<li class="badge">'.$mail['email'].'</li></br>'; ?>
+								</ul></td>
+							<td style="font-size: 9;"><ul id="files<?php echo $id; ?>">
+									<?php foreach($files as &$file) echo $file;?>
+								</ul></td>
+							<td align="center"><input type="checkbox" name="send[]"
+								id="send<?php echo $id; ?>" /></td>
+							<td align="center"><img src="img/ajax-loader.gif"
+								style="display: none;" id="load<?php echo $id; ?>" alt="loader" />
+							</td>
+							<td align="center">
+								<div class="btn-group btn-group-sm">
+									<button class="btn btn-default btn-lg"
+										onclick="javascript:changeRow('<?php echo $id; ?>');">
+										<span class="glyphicon glyphicon-pencil"></span>
+									</button>
+									<button class="btn btn-default btn-lg"
+										onclick="javascript:saveRow('<?php echo $id; ?>','save.php');">
+										<span class="glyphicon glyphicon-floppy-disk"></span>
+									</button>
+									<button class="btn btn-default btn-lg"
+										onclick="javascript:deleteRow('<?php echo $id; ?>','delete.php');"
+										class="delete">
+										<span class="glyphicon glyphicon-remove"></span>
+									</button>
+								</div>
+							</td>
+						</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+
+			</div>
+
+		</div>
+		<div class="container" style="display:none;" id="tabs-2">
+			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
+				<fieldset style="font-size: 12px">
+					<legend>Subir nuevo archivo</legend>
+					<div class="input-group">
+						<span class="input-group-btn"> <span
+							class="btn btn-primary btn-file"> Eligir Archivo ... <input
+								name="fichier" type="file" />
+						</span>
+						</span> <input type="text" readonly="" class="form-control" />
+					</div>
+					<span>Seleccionar un archivo con el formato ZIP.</span> 
+				</fieldset>
+				<br/>
+				<input type="submit" class="btn btn-lg btn-warning" value="Subir" />
+			</form>
+		</div>
+		<div class="container" style="display:none;" id="tabs-3">
+			<fieldset style="font-size: 12px">
+				<legend>Parametros</legend>
+
+				<form>
+					<div class="form-group">
+						<label for="from">E-mail</label> <input type="text"
+							value="<?php echo $From ?>" class="form-control" id="inputEmail"
+							name="from" />
+					</div>
+
+					<div class="form-group">
+						<label for="Password">Password</label> <input type="password"
+							class="form-control" name="Password"
+							value="<?php echo $Password ?>" />
+					</div>
+					<div class="form-group">
+						<label for="Body">Mesaje</label>
+						<textarea rows="15" class="form-control" cols="40" name="Body">
+			                <?php echo $Body ?>
+			            </textarea>
+					</div>
+					<button type="submit" class="btn btn-primary">Salvar</button>
+				</form>
+			</fieldset>
+		</div>
 </body>
 </html>
--- send.php
+++ send.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'Swift-5.0.3/lib/swift_required.php';
+
 if(isset($_POST['mails']) && isset($_POST['AttachedFiles'])) {
 	
 	$f = $_POST['AttachedFiles'];
@@ -10,6 +12,7 @@ if(isset($_POST['mails']) && isset($_POST['AttachedFiles'])) {
 	
 	if(count($recipientes) >= 1 && count($files) >= 1) {
 		
+	    /*
 		require("phpmailer/class.phpmailer.php");
 		
 		$mail = new PHPmailer();
@@ -50,6 +53,45 @@ if(isset($_POST['mails']) && isset($_POST['AttachedFiles'])) {
 		if(!$mail->Send()){ echo $mail->ErrorInfo; }
 	
 		unset($mail);
+		*/
+	    	    
+	    // Create the SMTP configuration
+	    $transport = Swift_SmtpTransport::newInstance("smtp.gmail.com", 465, 'ssl');
+	    $transport->setUsername("elteniente.computershop@gmail.com");
+	    $transport->setPassword("jotejote");
+	    
+	    // Create the message
+	    $message = Swift_Message::newInstance();	    
+	    $message->setTo(array_filter($recipientes,'strlen'));
+	    $message->setSubject($empresa);
+	    $message->setFrom('elteniente.computershop@gmail.com', 'COMPUTERSHOP');
+	    $message->setBody('-- 
+		Adjunto saldos y reporte.-
+
+		NOTA: Los saldos pueden tener un desfase de 48 hrs. aprox. con respecto a la última transacción.
+		Les recordamos hacer sus compras de  servicios para su empresa con anticipación.
+
+		Atte. Computershop.
+
+		Dudas, consultas y reclamos a los contactos:
+		Lucero-Gloria@aramark.cl
+		elteniente@computershop.cl');
+	    
+	    
+	    foreach ($files as $file) {
+	        $file = rawurldecode($file);
+	        if($file != '' && file_exists($file)) {
+	             $message->attach(Swift_Attachment::fromPath($file));
+	        }
+	    }
+	    
+	    // Send the email
+	    $mailer = Swift_Mailer::newInstance($transport);
+	    $mailer->send($message, $failedRecipients);
+	    
+	    // Show failed recipients
+	    echo empty($failedRecipients) ? '' : print_r($failedRecipients);
+	    
 	}
 }
 ?>
